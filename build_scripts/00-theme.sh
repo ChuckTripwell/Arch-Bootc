cd8ede51c590c11929473d419c2a54a7319350ff
ChuckTripwell <levif58@walla.co.il>
2025-11-14 10:30:02 +0000

    Update 00-theme.sh

--- build_scripts/00-theme.sh
+++ build_scripts/00-theme.sh
@@ -4,13 +4,5 @@ set -xeuo pipefail
 
 cp -avf "/ctx/files"/. /
 
-add_wants_niri() {
-    sed -i "s/\[Unit\]/\[Unit\]\nWants=$1/" "/usr/lib/systemd/user/niri.service"
-}
-add_wants_niri plasma-polkit-agent.service
-add_wants_niri udiskie.service
-add_wants_niri xwayland-satellite.service
-cat /usr/lib/systemd/user/niri.service
-
-systemctl enable greetd
+systemctl enable sddm
 
