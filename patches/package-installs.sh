c30924e1ea850bc17287fffc478342fbd47d2266
ChuckTripwell <levif58@walla.co.il>
2025-11-16 13:10:12 +0200

    Update package-installs.sh

--- patches/package-installs.sh
+++ patches/package-installs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-pacman -S --noconfirm cachyos-deckify-headers cachyos-deckify cachyos-handheld
+pacman -S --noconfirm cachyos-handheld cachyos-deckify-headers cachyos-deckify steam-powerbuttond-git steamos-manager jupiter-fan-control steamos-networking-tools 
 
-pacman -S --noconfirm plasma-desktop sddm plasma-pa plasma-nm micro fastfetch breeze kate ark scx-scheds scx-manager flatpak dolphin firewalld docker podman ptyxis
+pacman -S --noconfirm plasma-desktop sddm plasma-pa plasma-nm micro fastfetch breeze kate ark scx-scheds scx-manager flatpak dolphin firewalld docker podman distrobox alacritty
+# pacman -S --noconfirm ptyxis 
 
 #pacman -S --nocobfirm $( curl https://codeberg.org/Dwdeath/parent-lock_for_cachyos-handheld/raw/branch/main/Package_list.txt ) 
