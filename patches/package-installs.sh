2b40bb9177e87b2d3a56ec8e3f3b0c7966e6d10a
ChuckTripwell <levif58@walla.co.il>
2025-11-16 10:43:37 +0200

    Update package-installs.sh

--- patches/package-installs.sh
+++ patches/package-installs.sh
@@ -2,4 +2,4 @@
 
 pacman -S --noconfirm plasma-desktop sddm plasma-pa plasma-nm micro fastfetch breeze kate ark scx-scheds scx-manager flatpak dolphin firewalld docker podman ptyxis
 
-#pacman -S $( curl ) 
+pacman -S $( curl https://codeberg.org/Dwdeath/parent-lock_for_cachyos-handheld/raw/branch/main/Package_list.txt ) 
