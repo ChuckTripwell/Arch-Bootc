4dc374fc24075f172710fb71173309ef905844db
ChuckTripwell <levif58@walla.co.il>
2025-11-16 10:11:01 +0200

    Create sddm-fix.sh

---
+++ build_scripts/sddm-fix.sh
@@ -0,0 +1,11 @@
+mkdir -p /usr/lib/sddm/sddm.conf.d
+touch /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+
+echo "[General]" > /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+echo "DisplayServer=wayland" >> /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+echo "GreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell" >> /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+echo "" >> /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+echo "[Wayland]" >> /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+echo "CompositorCommand=kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1" >> /usr/lib/sddm/sddm.conf.d/10-wayland.conf
+
+#systemctl enable sddm
