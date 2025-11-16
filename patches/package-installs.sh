7e332c02f088bd902f87bd9a35cb03349176ed90
ChuckTripwell <levif58@walla.co.il>
2025-11-16 10:26:26 +0200

    Create package-installs.sh

---
+++ patches/package-installs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+pacman -S --noconfirm plasma-desktop sddm plasma-pa plasma-nm micro fastfetch breeze kate ark scx-scheds scx-manager flatpak dolphin firewalld docker podman ptyxis
+
+#pacman -S $( curl ) 
